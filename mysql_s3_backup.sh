29572f7fff3aba3e6cf65f940c021baeed7fc559
guilhermerodriguesti <57836353+guilhermerodriguesti@users.noreply.github.com>
2020-11-16 14:59:17 -0300

    Update mysql_s3_backup.sh

--- mysql_s3_backup.sh
+++ mysql_s3_backup.sh
@@ -11,14 +11,24 @@ BUCKET_NAME="bucket-backup-mysql"
 TSTAMP=$(date +%H%M%S)
 #current date/time YYY-MM-DD-hhmm
 TODAY=$(date +"%Y-%m-%d")
+
+log=/tmp/${BUCKET_NAME}-${TODAY}-${TSTAMP}.log
+echo "########################################################################################################" >> $log
+echo "########################################################################################################" >> $log
+echo "--------------------------------------------------------------------------------------------------------" >> $log
+echo " INICIADO - $data - $hora"                                                                                >> $log
+echo "--------------------------------------------------------------------------------------------------------" >> $log
+echo -e "\033[91m CRIANDO BUCKET BACKUP... \033[39m"                                                            >> $log
 aws s3 mb s3://$BUCKET_NAME
     #output directory
     OUTPUT_DIR="$BASE_DIR/$DB_NAME"
 
     #create the DB directory if it doesn't exist
     if [ ! -d $OUTPUT_DIR ]; then
+    echo -e "\033[91m CRIANDO DIRETORIO BACKUP... \033[39m"                                                     >> $log
         mkdir -p $OUTPUT_DIR
     fi
+    echo -e "\033[91m GERANDO BACKUP... \033[39m"                                                               >> $log
     cd $OUTPUT_DIR
     #mysqldump sql script
     mysqldump --host=$DB_HOST --user=$DB_USER --password=$DB_PASS --databases $DB_NAME > $DB_NAME.$TODAY-$TSTAMP.sql
@@ -26,6 +36,12 @@ aws s3 mb s3://$BUCKET_NAME
     if [ -f $DB_NAME.$TODAY-$TSTAMP.sql ]; then
         #zip to TODAY zip
         #zip -q $OUTPUT_DIR/$DB_NAME.sql.$TODAY-$TSTAMP.zip $DB_NAME.$TODAY-$TSTAMP.sql
+        echo -e "\033[91m SINCRONIZANDO ARQUIVO... \033[39m"                                                    >> $log
         aws s3 sync $BASE_DIR s3://$BUCKET_NAME
 
     fi
+echo "--------------------------------------------------------------------------------------------------------" >> $log
+echo " FINALIZADO - $data - $hora" >> $log
+echo "--------------------------------------------------------------------------------------------------------" >> $log
+echo "########################################################################################################" >> $log
+echo "########################################################################################################" >> $log
